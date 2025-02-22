76fc6c2f38c97c8dde9fde806e0f9bf9d74845b7
Moritz Mueller <moritz.j.mueller@gmail.com>
2025-02-22 04:30:02 +0100

    Add stars to end screen

--- Assets/Scripts/EndScreenManager.cs
+++ Assets/Scripts/EndScreenManager.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -14,6 +15,9 @@ public class EndScreenManager : MonoBehaviour
     public Vector2 orderShowPosition;
     public Vector2 stampPosition;
     public Vector2 orderSpawnPos;
+    public Sprite filledStarSprite;
+    public Sprite unFilledStarSprite;
+    public List<SpriteRenderer> stars;
     
     private ScreenTinter _screenTinter;
     
@@ -30,13 +34,17 @@ public class EndScreenManager : MonoBehaviour
         var seq = LeanTween.sequence();
 
         var situationCount = _situationGenerator.situations.Length;
-        
+
+        var completedOrders = 0;
         for (var situationIndex = 0; situationIndex < situationCount; situationIndex++)
         {
             var situation = _situationGenerator.situations[situationIndex];
             var situationCompleted = situation.car.GetComponentInChildren<CarController>().GetPathSize() ==
                                      situation.car.GetComponentInChildren<CarController>().GetPath()
                                          .Length; //This is not given because path size is the maximum simulated length
+            
+            if(situationCompleted)
+                completedOrders++;
             
             var orderPage = Instantiate(orderPagePrefab, transform);
             orderPage.transform.position = orderSpawnPos;
@@ -63,7 +71,7 @@ public class EndScreenManager : MonoBehaviour
             stamp.transform.localPosition = stampPosition + new Vector2(Random.Range(-0.5f, 0.5f), Random.Range(-0.5f, 0.5f));
             stamp.transform.rotation = Quaternion.Euler(0f, 0f, Random.Range(-20, 20));
             
-            seq.append(LeanTween.moveLocal(orderPage, orderShowPosition, 0.8f).setEase(LeanTweenType.easeOutExpo));
+            seq.append(LeanTween.moveLocal(orderPage, orderShowPosition, 0.3f).setEase(LeanTweenType.easeOutExpo));
             seq.append(() =>
             {
                 LeanTween.scale(stamp, Vector3.one * 1.5f, 0.4f).setEase(LeanTweenType.easeOutExpo);
@@ -76,10 +84,33 @@ public class EndScreenManager : MonoBehaviour
                 LeanTween
                     .moveLocal(orderPage,
                         Vector2.Lerp(ordersStartPos, ordersEndPos, index / (situationCount - 1f)),
-                        0.7f).setEase(LeanTweenType.easeInOutCubic);
+                        0.4f).setEase(LeanTweenType.easeInOutCubic);
                 LeanTween.scale(orderPage, orderScaleInLineUp, 0.3f).setEase(LeanTweenType.easeInOutCubic);
             });
-            seq.append(0.7f);
+            seq.append(0.4f);
+        }
+
+        var score = completedOrders / (float)situationCount; //Score is 70 percent here
+
+        for (int starIndex = 0; starIndex < stars.Count; starIndex++)
+        {
+            var star = stars[starIndex];
+            var starPrecentage = ((1f / stars.Count) * (starIndex + 1)) - 0.01f;
+            if (score >= starPrecentage)
+            {
+                var starScale = star.transform.localScale;
+                star.sprite = filledStarSprite;
+                seq.append(() => { star.enabled = true; });
+                seq.append(LeanTween.scale(star.gameObject, starScale * 1.2f, 0.2f).setEase(LeanTweenType.easeInOutCubic));
+                seq.append(LeanTween.scale(star.gameObject, starScale, 0.3f).setEase(LeanTweenType.easeInOutCubic));
+            }
+            else
+            {
+                star.sprite = unFilledStarSprite;
+                star.enabled = true;
+                star.color = Color.clear;
+                seq.append(LeanTween.color(star.gameObject, Color.white, 0.5f).setEase(LeanTweenType.easeInOutCubic));
+            }
         }
     }
 }
